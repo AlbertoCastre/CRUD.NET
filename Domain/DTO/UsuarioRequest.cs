0338a0558cd9d126e1654a976936327d818a17d9
Alberto Castrejón Hernández <betotenoch@gmail.com>
2025-05-24 20:51:44 -0500

    primer commir jeje

---
+++ Domain/DTO/UsuarioRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace Domain.DTO
+{
+    public class UsuarioRequest
+    {
+        public string Nombre { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public int? FkRol { get; set; }
+    }
+}
